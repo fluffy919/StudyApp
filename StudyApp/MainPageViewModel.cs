776649d8e7c3cac1abe7ae72f24d40708dccca50
David <david@davidevelop.com>
2022-03-11 12:50:16 -0500

    begin MVVM

---
+++ StudyApp/MainPageViewModel.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.ComponentModel;
+using Xamarin.Forms;
+using System.Text;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace StudyApp
+{
+    public class MainPageViewModel : INotifyPropertyChanged
+    {
+        public MainPageViewModel()
+        {
+            EraseCommand = new Command(() =>
+            {
+                TheNote = string.Empty;
+            });
+
+            SaveCommand = new Command(() =>
+            {
+                AllNotes.Add(TheNote);
+
+                TheNote = string.Empty;
+            });
+        }
+
+        public ObservableCollection<string> AllNotes { get; set; } = new ObservableCollection<string>();
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        string theNote;
+
+        public string TheNote
+        {
+            get => theNote;
+            set
+            {
+                theNote = value;
+
+                var args = new PropertyChangedEventArgs(nameof(TheNote));
+
+                PropertyChanged?.Invoke(this, args);
+            }
+        }
+
+        public Command SaveCommand { get; }
+
+        public Command EraseCommand { get; }
+    }
+}
